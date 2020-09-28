4573cc028bedff6cfc286b2714912243f8f0a3bb
mgregus <56076866+mgregus@users.noreply.github.com>
2020-09-28 15:20:32 +0200

    c source file

---
+++ cmake-build-debug/CMakeFiles/zadanie1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie1.dir/main.c.obj"
+  "zadanie1.exe"
+  "zadanie1.exe.manifest"
+  "zadanie1.lib"
+  "zadanie1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zadanie1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <npcap.h>
+#include <winpcap.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
