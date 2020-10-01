a39d7f3517e56f1810e029145fcde8c52901acba
mgregus <56076866+mgregus@users.noreply.github.com>
2020-10-01 09:56:11 +0200

    konecne funckne pcap

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <npcap.h>
-#include <winpcap.h>
+#include <stdlib.h>
+#include <pcap.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
 }
