5b0b21d401f4bc6c3681ea8f81c8de33c39f82c4
mgregus <56076866+mgregus@users.noreply.github.com>
2020-10-04 17:48:03 +0200

    1 treba dokoncit identifikaciu IEEE

--- main.c
+++ main.c
@@ -7,6 +7,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+typedef struct Ethernet{
+	u_char Dmac[6];
+	u_char Smac[6];
+	u_char type[2];
+}Ethernet;
+
+typedef struct IEEEraw{
+	u_char Dmac[6];
+	u_char Smac[6];
+	u_char len[2];
+	u_char ipx[3];
+}IEEEraw;
+
+typedef struct IEEEllc{
+	u_char Dmac[6];
+	u_char Smac[6];
+	u_char len[2];
+	u_char dsap[1];
+	u_char ssap[1];
+	u_char cont[1];
+}IEEEllc;
+
+typedef struct IEEEsnap{
+	u_char Dmac[6];
+	u_char Smac[6];
+	u_char len[2];
+	u_char dsap[1];
+	u_char ssap[1];
+	u_char cont[1];
+	u_char vc[3];
+	u_char type[2];
+}IEEEsnap;
+
+
 
 int dlzka_paketu_po_mediu(int apilength){
 	int medialength = 0;
@@ -25,7 +59,7 @@ u_char *copyuchar(u_char *source, int n){
 	u_char *dest;
 	dest = malloc(n*sizeof(u_char));
 	for(i = 0; i < n; i++){
-		printf("%.2x ",source[i]);
+		//printf("%.2x ",source[i]);
 		dest[i] = source[i];
 	}	
 
@@ -35,6 +69,40 @@ u_char *copyuchar(u_char *source, int n){
 u_short hex_to_dec(u_char *x){
 	u_short decimal = *(u_short*)x;
 	return decimal = (decimal >> 8) | ((decimal & 255) << 8);;	
+}
+
+//postupnost bajtov zmeni na desiatkove cislo, funguje len na 4B
+int hodnota(u_char *pole, int n){
+	int i,j,iter;
+	iter = 0;
+	i = 0;
+	
+	for(iter = 0; iter < n ; iter++){
+		i = i << 8;
+		i += (int)pole[iter];
+	}
+	
+
+	return i;
+}
+
+
+void vypisMacadries(Ethernet *pt, FILE *output){
+			
+			int i;
+			
+			fprintf(output,"Cielová MAC adresa: ");
+			for(i = 0; i < 6; i++)
+			fprintf(output,"%.2x ",pt->Dmac[i]);
+			fprintf(output,"\n");
+			
+			fprintf(output,"Zdrojová MAC adresa: ");
+			for(i = 0; i < 6; i++)
+			fprintf(output,"%.2x ",pt->Smac[i]);
+			fprintf(output,"\n");
+		
+			
+			
 }
 
 int main(int argc, char *argv[]) {
@@ -56,11 +124,14 @@ int main(int argc, char *argv[]) {
 	strncat(filepath, filename, filenamelength);
 	//printf("%s ",filepath);
 	int modvypisu = 0;
+	//***********************************************************************************
 	
 	//pomocne .txt subory
-	
 	FILE *output;
 	output = fopen("output.txt","w");
+	FILE *protokoly;
+	protokoly = fopen("protokoly.txt","r");
+	//***********************************************************************************
 	
 	//vyber modu vypisu
 	printf("zadajte mod vypisu: \n");
@@ -71,18 +142,35 @@ int main(int argc, char *argv[]) {
 	const u_char *data_packetu;
 	pcap_t *pcap_subor;	
 	pcap_subor = pcap_open_offline(filepath, chyba_packet_suboru);
+	//***********************************************************************************
+	
+	//pomocne premmenne
+	u_char *pom;
+	int decimalvalue;
+	//***********************************************************************************
+	
+	
+	//l2 struktury
+	Ethernet *ethernet = malloc(sizeof(Ethernet));
+	IEEEraw *ieee = malloc(sizeof(IEEEraw));
+	IEEEllc *ieeellc = malloc(sizeof(IEEEllc));
+	IEEEsnap *ieeesnap = malloc(sizeof(IEEEsnap));
+	//***********************************************************************************
 	
 	//otvaranie potrebnych suborov
 	if(pcap_subor == NULL){
 		printf("Chyba pri otvarani packet suboru: %s\n",chyba_packet_suboru);
 		return 1;
 	}
+	//***********************************************************************************
 	
 	if(modvypisu == 1){
 	
 		int porcisloramca = 0;
-		while(pcap_next_ex(pcap_subor,&hlavicka_packetu, &data_packetu) == 1/* && porcisloramca < 5*/){
+		while(pcap_next_ex(pcap_subor,&hlavicka_packetu, &data_packetu) == 1 /*&& porcisloramca < 5*/){
+			
 			porcisloramca++;
+			
 			fprintf(output,"ramec: %d\n",porcisloramca);
 			fprintf(output,"dlzka poskytnuta pcap API - %d B\n",hlavicka_packetu->caplen);
 			fprintf(output,"dlzka prenasana po mediu - %d B\n",dlzka_paketu_po_mediu(hlavicka_packetu->caplen));
@@ -90,34 +178,24 @@ int main(int argc, char *argv[]) {
 			//printf("decimal: %d\n",hex_to_dec("8c6F",4));
 			
 			//zistenie typu L2 ramca
-			int pom = 0;
-			int i;
-			for(i = 12; i < 14; i++)
-				L2protokol[pom++] = data_packetu[i]; 
+			ethernet = (Ethernet*)(data_packetu);
+			ieee = (IEEEraw*)(data_packetu);
+			ieeellc = (IEEEllc*)(data_packetu);
+			ieeesnap = ((IEEEsnap*)data_packetu);
 			
-			//Ramec L2 vrstvy je Ethernet
-			if(hex_to_dec(L2protokol) > 1500){
-				fprintf(output,"Ethernet II\n");
-			}			
-			//Ramec L2 vrstvy je IEEE 802.3
+			
+			pom = copyuchar((u_char*)data_packetu+12, 2);
+			
+			decimalvalue = hodnota(pom, 2);
+				//printf("%d\n",decimalvalue);
+			if(decimalvalue > 1500){
+				fprintf(output,"Ethernet\n");
+			}
 			else{
 				fprintf(output,"IEEE 802.3\n");
 			}
 			
-			//zistenie MAC adries
-			fprintf(output,"Cielová MAC adresa: ");
-			for(i = 0 ; i < 6; i++){
-				fprintf(output,"%.2x ",data_packetu[i]);
-			}
-			fprintf(output,"\n");
-			fprintf(output,"Zdrojová MAC adresa: ");
-			for(i = 6 ; i < 12; i++){
-				fprintf(output,"%.2x ",data_packetu[i]);
-			}
-			fprintf(output,"\n");
-			
-			
-				
+			vypisMacadries(ethernet,output);	
 				
 			//vypis ramca
 			int it = 0;
